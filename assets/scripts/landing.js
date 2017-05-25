4b5f1fddaf43d4339b12fd1d8b488d7183d29596
DESKTOP-VSVB76H\Admin <kreyes616@gmail.com>
2017-05-24 21:27:18 -0400

    finish with checkpoint 23

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,7 +1,7 @@
 
-var animatePoints = function(className) {
-  var points = document.getElementsByClassName(className);
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
   for (var p = 0; p < points.length; p++) {
       points[p].style.opacity = 1;
       points[p].style.transform = 'scaleX(1) translateY(0)';
@@ -9,3 +9,19 @@ var animatePoints = function(className) {
       points[p].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
 };
+
+window.onload = function() {
+
+if (window.innerHeight > 950) {
+  animatePoints(pointsArray);
+}
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scroolDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scroolDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
